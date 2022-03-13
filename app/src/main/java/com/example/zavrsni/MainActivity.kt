0fa6fb27d7ca1067d787306b79a3db1aa58e9cbd
osaric1 <osaric1@etf.unsa.ba>
2022-03-13 19:58:30 +0100

    fingerprint

--- app/src/main/java/com/example/zavrsni/MainActivity.kt
+++ app/src/main/java/com/example/zavrsni/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.zavrsni
 import androidx.biometric.BiometricManager;
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +18,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        BiometricManager.from(this).canAuthenticate()
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, object: BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(applicationContext, "Authentication error: $errString", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(applicationContext, "Authentication succeeded!", Toast.LENGTH_SHORT).show()
+            }
+
+
+        })
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        val biometricLoginButton = findViewById<Button>(R.id.biometric_login)
+        biometricLoginButton.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
     }
 
 }
