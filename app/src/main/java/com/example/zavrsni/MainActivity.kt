7c3ebea5ad2e94ea811c844548062b1ead2ac886
osaric1 <osaric1@etf.unsa.ba>
2022-03-22 22:49:13 +0100

    miscellanious

--- app/src/main/java/com/example/zavrsni/MainActivity.kt
+++ app/src/main/java/com/example/zavrsni/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.zavrsni
 
-import androidx.biometric.BiometricManager;
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.*
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import java.util.concurrent.Executor
@@ -13,11 +19,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
     private lateinit var executor: Executor
+    private final var REQUEST_CODE: Int = 12
 
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val biometricManager = BiometricManager.from(this)
+
         executor = ContextCompat.getMainExecutor(this)
         biometricPrompt = BiometricPrompt(this, executor, object: BiometricPrompt.AuthenticationCallback(){
             override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
@@ -25,23 +35,61 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Authentication error: $errString", Toast.LENGTH_SHORT).show()
             }
 
+
             override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
                 super.onAuthenticationSucceeded(result)
                 Toast.makeText(applicationContext, "Authentication succeeded!", Toast.LENGTH_SHORT).show()
             }
 
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(applicationContext, "Authentication failed",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
 
         })
         promptInfo = BiometricPrompt.PromptInfo.Builder()
             .setTitle("Biometric login for my app")
             .setSubtitle("Log in using your biometric credential")
-            .setNegativeButtonText("Use account password")
+            //.setNegativeButtonText("Use account password")
+            .setConfirmationRequired(false)
+            .setDeviceCredentialAllowed(true)
             .build()
 
         val biometricLoginButton = findViewById<Button>(R.id.biometric_login)
         biometricLoginButton.setOnClickListener {
             biometricPrompt.authenticate(promptInfo)
         }
+
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            // Can't call setNegativeButtonText() and
+            // setAllowedAuthenticators(... or DEVICE_CREDENTIAL) at the same time.
+            .setNegativeButtonText("Use account password")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG  or BIOMETRIC_WEAK)
+            .setConfirmationRequired(false)
+            .setDeviceCredentialAllowed(true)
+            .build()
+
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                Log.d("Message", "App can authenticate using biometrics.")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Log.e("Message", "No biometric features available on this device.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Log.e("Message", "Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                    putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+                }
+                startActivityForResult(enrollIntent, REQUEST_CODE)
+            }
+        }
+
     }
 
 }
